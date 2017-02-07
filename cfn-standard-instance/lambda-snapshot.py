4231d42bc8cef8f3fc114a1f685d42456cf6c2e6
Michael Ludvig <mludvig@logix.net.nz>
2017-02-07 15:15:07 +1300

    lambda-snapshot.py - Refactor to a separate function, better logging.

--- cfn-standard-instance/lambda-snapshot.py
+++ cfn-standard-instance/lambda-snapshot.py
@@ -13,14 +13,9 @@ from datetime import datetime
 
 ec2 = boto3.client('ec2')
 
-def lambda_handler(event, context):
-    try:
-        instance_id = os.environ['instance_id']
-    except:
-        print('Environment variable [instance_id] must be set')
-        raise
-
-    print('Snapshotting instance: %s' % instance_id)
+def create_image(instance_id):
+    snapshot_timestamp = datetime.strftime(datetime.now(), '%s')
+    print('%s @ %s: Snapshotting instance' % (instance_id, snapshot_timestamp))
     instance = ec2.describe_instances(InstanceIds=[instance_id])
     description = ''
     tags = {}
@@ -37,10 +32,9 @@ def lambda_handler(event, context):
     else:
         description = instance_id
 
-    snapshot_timestamp = datetime.strftime(datetime.now(), '%s')
     name = instance_id + '_' + snapshot_timestamp
     description = description + ' ' + datetime.strftime(datetime.now(), '%Y-%m-%d %H-%M-%S')
-    print('Creating image: %s' % name)
+    print('%s @ %s: Creating image: %s' % (instance_id, snapshot_timestamp, name))
     response = ec2.create_image(
         InstanceId = instance_id,
         Name = name,
@@ -48,7 +42,7 @@ def lambda_handler(event, context):
         NoReboot = False,
     )
     image_id = response['ImageId']
-    print('Created Image: %s' % image_id)
+    print('%s @ %s: Created Image: id=%s [%s]' % (instance_id, snapshot_timestamp, image_id, name))
     image_tags = [
         {'Key': 'SnapshotTimestamp', 'Value': snapshot_timestamp },
         {'Key': 'InstanceId', 'Value': instance_id }
@@ -56,5 +50,18 @@ def lambda_handler(event, context):
     if 'Name' in tags:
         image_tags.append({ 'Key': 'Name', 'Value': tags['Name'] })
     ec2.create_tags(Resources = [image_id], Tags = image_tags)
+    image_tags_string = ', '.join(map(lambda x: '%(Key)s=%(Value)s' % x, image_tags))
+    print('%s @ %s: Created Tags: %s' % (instance_id, snapshot_timestamp, image_tags_string))
+
+    return (image_id, snapshot_timestamp)
+
+def lambda_handler(event, context):
+    try:
+        instance_id = os.environ['instance_id']
+    except:
+        print('Environment variable [instance_id] must be set')
+        raise
+
+    image_id, snapshot_timestamp = create_image(instance_id)
 
     return image_id
